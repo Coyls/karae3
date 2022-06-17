f37c1b2fc568fa308637587e019bc8b8f1fd4681
Coyls <loic.grecofr@gmail.com>
2022-06-17 10:00:29 +0200

    refactor(expo): Some fix and change voice without connection

--- utils/speak.py
+++ utils/speak.py
@@ -5,11 +5,14 @@ import gtts
 from playsound import playsound
 
 class Speak:
+    # Echanger les fonctions en fonction de en ligne ou non
+    # premiere : HORS LIGNE
+    # Deuxieme : EN LIGNE
 
-    def speak2(txt: str):
+    def speak(txt: str):
         subprocess.run(["sh","./scripts/speak.sh",txt])
 
-    def speak(txt: str): # ja
+    def speak2(txt: str): # ja
         tts = gtts.gTTS(txt, lang="fr")
         tts.save("./audio/txt.mp3")
         playsound("./audio/txt.mp3")
